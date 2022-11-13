47758560021e4be104d083c9f99a8d7aa52e3d60
RoboticCat7000 <rajathsukeertib@gmail.com>
2022-11-13 14:23:44 +0530

    Fixed

--- components/Rotation.js
+++ components/Rotation.js
@@ -46,41 +46,37 @@ AFRAME.registerComponent("plane-rotation-reader", {
       this.data.speedOfAscent = this.el.getAttribute("position");
       var planeRotation = this.data.speedOfRotation;
       var planePosition = this.data.speedOfAscent;
-      if(e.key === "ArrowRight"){
-        if(planeRotation.x < 10){
-            planeRotation.x += 0.5
-            this.el.setAttribute("rotation",planeRotation)
+      if (e.key === "ArrowRight") {
+        if (planeRotation.x < 10) {
+          planeRotation.x += 0.5;
+          this.el.setAttribute("rotation", planeRotation);
         }
       }
-      if(e.key === "ArrowLeft"){
-        if(planeRotation.x > -10){
-            planeRotation.x -= 0.5
-            this.el.setAttribute("rotation",planeRotation)
+      if (e.key === "ArrowLeft") {
+        if (planeRotation.x > -10) {
+          planeRotation.x -= 0.5;
+          this.el.setAttribute("rotation", planeRotation);
         }
       }
-      if(e.key === "ArrowUp"){
-        if(planeRotation.z < 20){
-            planeRotation.z -= 0.5
-            this.el.setAttribute("rotation",planePosition)
+      if (e.key === "ArrowUp") {
+        if (planeRotation.z < 20) {
+          planeRotation.z += 0.5;
+          this.el.setAttribute("rotation", planeRotation);
         }
-        if(planePosition.y < 2){
-            planePosition.y += 0.01
-            this.el.setAttribute("positon",planePosition)
+        if (planePosition.y < 2) {
+          planePosition.y += 0.01;
+          this.el.setAttribute("positon", planePosition);
         }
-
-
       }
-      if(e.key === "ArrowDown"){
-        if(planeRotation.z > -10){
-            planeRotation.z -= 0.5
-            this.el.setAttribute("rotation",planeRotation)
+      if (e.key === "ArrowDown") {
+        if (planeRotation.z > -10) {
+          planeRotation.z -= 0.5;
+          this.el.setAttribute("rotation", planeRotation);
         }
-        if(planePosition.y > -2){
-            planePosition.y -= 0.01
-            this.el.setAttribute("position",planePosition)
+        if (planePosition.y > -2) {
+          planePosition.y -= 0.01;
+          this.el.setAttribute("position", planePosition);
         }
-
-
       }
     });
   },
